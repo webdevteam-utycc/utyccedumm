84f173d0a639a86d645c73d4749c81ea8ed103e7
zawwai1997 <mamahninsu.ioio@gmail.com>
2020-09-11 10:33:51 +0630

    ui fixed cards

---
+++ includes/conference-right-cards.php
@@ -0,0 +1,68 @@
+<div class="flex-item fi-lg-3x fi-md-3x fi-sm-12x">
+    <div class="container-fluid">
+        <h3>Conference Chair</h3>
+        <div class="flex-box">
+            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
+                <div class="container-fluid card"
+                     style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
+                    <div class="text-center">
+                        <p style="font-size: 1.25rem; color:#fff;">Prof. Aung Win<br>
+                            <span style="font-size: 1rem;">Rector<br>
+                        <span style="font-size: 1rem;">University of Technolgy <br>(Yatanarpon Cyber City)</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h3>Conference Co-Chair</h3>
+        <div class="flex-box">
+            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
+                <div class="container-fluid card"
+                     style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
+                    <div class="text-center">
+                        <p style="font-size: 1.25rem; color:#fff;">Prof. Pyke Tin<br>
+                            <span style="font-size: 1rem;">University of Miyazaki <br>Japan</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h3></h3>
+        <div class="flex-box">
+            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
+                <div class="container-fluid card"
+                >
+                    <div class="text-center">
+                        <p style="font-size: 1.2rem;padding: 10px 0px;"><a href="testing-5164.php"
+                                                                           class="paper"
+                            >CONFERENCE
+                                ORGANIZING COMMITTEE</a> <br><br>
+                            <a href="testing-5165.php" class="paper">STEERING
+                                COMMITTEE</a> <br><br>
+                            <a
+                                href="testing-5166.php" class="paper">TECHCNICAL
+                                PROGRAM
+                                COMMIITTEE</a></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h3>Technical Program Committee Chair</h3>
+        <div class="flex-box">
+            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
+                <div class="container-fluid card"
+                     style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
+                    <div class="text-center">
+
+                        <p style="font-size: 1.25rem; color:#fff;">Prof. Reenu<br>
+                            <span style="font-size: 1rem;">PO. Box. 05087</span><br>
+                            <span style="font-size: 1rem;">Pyin Oo Lwin Township,University of Technology (Yatanarpon Cyber City), Mandalay, Myanmar</span><br><br>
+                            <span style="font-size: 1rem;"><span class="ui-icons ui-1_email-85"></span> reenu@utycc.edu.mm</span><br>
+                            <span style="font-size: 1rem;">09256265029</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
--- testing-5163.php
+++ testing-5163.php
@@ -39,13 +39,14 @@
                         <div class="unit">
                             <h3>Call for Papers</h3>
                             <p class="text-justify">The 1<sup>st</sup> International Research Conference on Science and
-                                Engineering (IRCSE) will be held at UTYCC, Pyin Oo Lwin, Myanmar on 11<sup>st</sup>
-                                January 2021 organized by the University of Technology (Yatanarpon Cyber City) under the
-                                Ministry of Education. It will cover the area of research and development in the fields
-                                of Science, Technology and Engineering. The conference offers a great opportunity to
-                                learn and share the knowledge and result in theory, methodology and innovation in
-                                science and engineering with leading experts in these areas and practices across the
-                                globe.
+                                Engineering (IRCSE) will be held at UTYCC, Pyin Oo Lwin, Myanmar on 11st January 2021
+                                organized by the University of Technology (Yatanarpon Cyber City) under the Ministry of
+                                Education. It is being organized in cooperation between University of Technology
+                                (Yatanarpon Cyber City) and University of Miyazaki, Japan. It will cover the area of
+                                research and development in the fields of Science, Technology and Engineering. The
+                                conference offers a great opportunity to learn and share the knowledge, technological
+                                advances and research findings in theoretical, experimental and innovation in science
+                                and engineering with leading experts and practices across the globe.
                             </p>
                         </div>
                         <h3>Topics of Conference</h3>
@@ -69,7 +70,7 @@
                                         <li>Engineering Mathematics</li>
                                         <li>Engineering Chemistry</li>
                                         <li>Engineering Education</li>
-                                        <li>Literature (Myanmar & English)</li>
+                                        <li>Linguistic and Literature</li>
                                     </ul>
                                 </div>
                             </div>
@@ -88,15 +89,14 @@
                                         <li>Engineering Mathematics</li>
                                         <li>Engineering Chemistry</li>
                                         <li>Engineering Education</li>
-                                        <li>Literature (Myanmar & English)</li>
+                                        <li>Linguistic and Literature</li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
                         <div class="unit">
                             <h3>Paper Submission Guidelines</h3>
-                            <p class="text-justify">Authors are invited to submit papers by 15<sup>th</sup> November,
-                                2021.
+                            <p class="text-justify">Authors are invited to submit papers by 15st November, 2020.
                                 Submitted papers must be original and have not been previously accepted in any journal
                                 or publication elsewhere. The paper must be no more than 6 pages. The paper format and
                                 author's approvement can be download here.
@@ -112,7 +112,7 @@
                                     <tr>
                                         <td>Submission Deadline</td>
                                         <td>
-                                            <del>15<sup>th</sup> December 2019</del>
+                                            <del style="color:#856404;">15<sup>th</sup> December 2019</del>
                                             </span>
                                             <br>
                                             15<sup>th</sup> November 2020
@@ -121,7 +121,7 @@
                                         <td>Local Participant<br>
                                             Date
                                         </td>
-                                        <td> 50000 Ks <br> 5<sup>th</sup> January 2021</td>
+                                        <td> 60000 Ks <br> 5<sup>th</sup> January 2021</td>
                                     </tr>
                                     <tr>
                                         <td>Acceptance Notification</td>
@@ -139,7 +139,7 @@
                                     </tr>
                                     <tr>
                                         <td>Conference Dates</td>
-                                        <td>23<sup>rd</sup> January 2020</td>
+                                        <td>11<sup>st</sup> January 2021</td>
                                         <td>Account</td>
                                         <td> DAW THU ZAR TINT
                                             <br>KBZ (10 430 199 914 344 501)
@@ -156,7 +156,7 @@
                                     <tr>
                                         <td>Submission Deadline</td>
                                         <td>
-                                            <del>15<sup>th</sup> December 2019</del>
+                                            <del style="color:#856404;">15<sup>th</sup> December 2019</del>
                                             </span>
                                             <br>
                                             15<sup>th</sup> November 2020
@@ -173,7 +173,7 @@
                                     </tr>
                                     <tr>
                                         <td>Conference Dates</td>
-                                        <td>23<sup>rd</sup> January 2020</td>
+                                        <td>11<sup>st</sup> January 2021</td>
                                     </tr>
                                 </table>
                                 <br>
@@ -186,7 +186,7 @@
                                         <td>Local Participant<br>
                                             Date
                                         </td>
-                                        <td> 50000 Ks <br> 5<sup>th</sup> January 2021</td>
+                                        <td> 60000 Ks <br> 5<sup>th</sup> January 2021</td>
                                     </tr>
                                     <tr>
                                         <td>Foreign Participant</td>
@@ -207,13 +207,14 @@
 
                             <div class="flex-box">
                                 <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
-                                    <div class="container-fluid card" style="background: linear-gradient(45deg,#FFB64D,#ffcb80);">
-                                        <h4 class="text-white">Special Announcement: The 1st IRCSE will be held
+                                    <div class="container-fluid alert"
+                                         style="background-color: #fff3cd; border-color:#ffeeba; border-radius: .25rem;">
+                                        <h4 style="color:#856404;">Special Announcement: The 1st IRCSE will be held
                                             Online</h4>
 
-                                        <p>Due to continued uncertainties surrounding the
-                                            ongoing global coronavirus pandemic, the organizing committee has made the
-                                            decision to conduct the 1st IRCSE entirely online via Zoom.<br>
+                                        <p style="color: #856404">Due to continued uncertainties surrounding the ongoing global coronavirus
+                                            pandemic, the organizing committee has made the decision to conduct the 1st
+                                            IRCSE entirely online via Zoom.<br>
                                     </div>
                                 </div>
                             </div>
@@ -226,67 +227,7 @@
                     </div>
 
                 </div>
-                <div class="flex-item fi-lg-3x fi-md-3x fi-sm-12x">
-                    <div class="container-fluid">
-                        <h3>General Chair</h3>
-                        <div class="flex-box">
-                            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
-                                <div class="container-fluid card" style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
-                                    <div class="text-center" >
-                                        <p style="font-size: 1.25rem; color:#fff;">Prof. Aung Win<br>
-                                            <span style="font-size: 1rem;">Rector<br>
-                        <span style="font-size: 1rem;">University of Technolgy <br>(Yatanarpon Cyber City)</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <h3>General Co-Chair</h3>
-                        <div class="flex-box">
-                            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
-                                <div class="container-fluid card" style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
-                                    <div class="text-center">
-                                        <p style="font-size: 1.25rem; color:#fff;">Prof. Pyke Tin<br>
-                                            <span style="font-size: 1rem;">University of Miyazaki <br>Japan</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <h3></h3>
-                        <div class="flex-box">
-                            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
-                                <div class="container-fluid card" style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
-                                    <div class="text-center">
-                                        <p style="font-size: 1.2rem;padding: 10px 0px;"><a href="testing-5164.php"
-                                                                                           class="paper" style="color:#fff;">CONFERENCE
-                                                ORGANIZING COMMITTEE</a> <br><br>
-                                            <a href="testing-5165.php" class="paper" style="color:#fff;">STEERING COMMITTEE</a> <br><br>
-                                            <a
-                                                    href="testing-5166.php" class="paper" style="color:#fff;">TECHCNICAL PROGRAM
-                                                COMMIITTEE</a></p>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <h3>Technical Program Committee Chair</h3>
-                        <div class="flex-box">
-                            <div class="flex-item fi-lg-12x fi-md-12x fi-sm-12x committee_container">
-                                <div class="container-fluid card" style="background: linear-gradient(45deg,#4099ff,#73b4ff);">
-                                    <div class="text-center">
-
-                                        <p style="font-size: 1.25rem; color:#fff;">Prof. Reenu<br>
-                                            <span style="font-size: 1rem;">PO. Box. 05087</span><br>
-                                            <span style="font-size: 1rem;">Pyin Oo Lwin Township,University of Technology (Yatanarpon Cyber City), Mandalay, Myanmar</span><br><br>
-                                            <span style="font-size: 1rem;"><span class="ui-icons ui-1_email-85"></span> reenu@utycc.edu.mm</span><br>
-                                            <span style="font-size: 1rem;">09256265029</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                <?php include('includes/conference-right-cards.php'); ?>
             </div>
         </div>
     </div>
