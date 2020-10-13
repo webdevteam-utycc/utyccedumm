3cb9439011b400d22646cdbfcb3195713e569b27
zawwai1997 <mamahninsu.ioio@gmail.com>
2020-10-13 10:18:26 +0630

    date fixed:

--- includes/ircse/ircse-upper-page.php
+++ includes/ircse/ircse-upper-page.php
@@ -4,7 +4,7 @@
     </div>
     <div class="flex-item fi-lg-8x fi-md-8x fi-sm-8x">
         <h2 class="ircse_ttl">The 1<sup>st</sup> International Research Conference on Science & Engineering</h2>
-        <p class="ircse_sub_ttl">11<sup>st</sup> January, 2021</p>
+        <p class="ircse_sub_ttl">11<sup>th</sup> January, 2021</p>
         <p class="ircse_sub_ttl">University of Technology (Yatanarpon Cyber City), UTYCC, Myanmar
             <img src="img/ircse4.png" altl="" class="ircse-container" >
     </div>
